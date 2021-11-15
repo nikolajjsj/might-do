8ec800798cd222fe1e5ed6268946b670bb48d6e3
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-11-15 20:21:11 +0100

    Implement task-item, with animated label and checkbox

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,15 +1,22 @@
-import * as React from 'react'
+import React, { useCallback, useState } from 'react'
+import { Pressable } from 'react-native'
 import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
-import AnimatedCheckbox from '../components/animated-checkbox'
+import TaskItem from '../components/task-item'
+// import AnimatedCheckbox from '../components/animated-checkbox'
 
 export default function MainScreen() {
+  const [checked, setChecked] = useState(false)
+  const handlePressCheckbox = useCallback(() => {
+    setChecked(prev => !prev)
+  }, [])
+
   return (
     <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
       <VStack space={5} alignItems="center">
-        <Box w="100px" h="100px">
-          <AnimatedCheckbox />
-        </Box>
+        <Pressable onPress={handlePressCheckbox}>
+          <TaskItem isDone={checked} onToggleCheckBox={handlePressCheckbox} />
+        </Pressable>
         <Box p={8} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>Hello</Text>
         </Box>
